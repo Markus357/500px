255c9500585b4654ebcc87187d46bb920cd6317d
Markus357 <markdavidson@snapd.com>
2018-11-28 23:26:19 -0500

    Remove App.css, refactor styles as styled components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components'
 
 import { ImagesProvider } from './providers/ImagesProvider';
+import PhotoShowcase from './components/PhotoShowcase';
 
-import './App.css';
+const GlobalStyles = createGlobalStyle`
+  html, body {
+    font-size: 62.5%; /* base 10px */
+    font-family: 'Helvetica Neue', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  }
+`;
 
-class App extends Component {
-  render() {
-    return (
-      <ImagesProvider>
+const AppStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const HeaderStyles = styled.header`
+  display: flex;
+  flex: 1;
+
+  padding: 2rem;
+  border-bottom: 1px solid #eee;
+
+  .logo {
+    height: 4rem;
+  }
+`;
+
+const App = () => (
+  <>
+    <GlobalStyles />
+
+    <ImagesProvider>
+      <AppStyles>
+
+        <HeaderStyles>
+          <img className="logo" src="logo.svg" alt="500px Logo" />
+        </HeaderStyles>
         <div className="App">
           <header className="App-header">
             <img className="App-logo" src="logo.svg" />
           </header>
-            
-        </div>
-      </ImagesProvider>
-    );
-  }
-}
+
+      </AppStyles>
+    </ImagesProvider>
+  </>
+);
 
 export default App;
